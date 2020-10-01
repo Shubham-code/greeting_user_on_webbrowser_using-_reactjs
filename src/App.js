70c10bd465b1a49f2d9042203e11c6cee162b8b8
Shubham <shubhamjain428@gmail.com>
2020-10-01 15:12:28 +0530

    greeting_user_in_webbrowser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = ()=> {
+  let currDate = new Date();
+  currDate = currDate.getHours();
+  let greeting = "";
+  const cssStyle = {};
+
+  if(currDate >= 1 && currDate < 12)
+  {
+    greeting = "Good Morning";
+    cssStyle.color = 'green';
+  } else if(currDate>=12 && currDate < 19)
+  {
+    greeting = "Good Afternoon";
+    cssStyle.color = 'Orange';
+  } else
+  {
+    greeting = "Good Night"
+    cssStyle.color = 'red';
+  }  
+  
+  return(
+    <>
+    <div>
+      <h1> 
+        Hello Sir, <span style={cssStyle}>{greeting}</span></h1>
     </div>
+    </>
   );
 }
 
